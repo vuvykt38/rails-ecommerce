0dd18eca2be7f9ba91516571587b2adb2e1c66d8
Vy <vuvykt38@gmail.com>
2021-02-20 17:08:48 +0700

    implement a down for previous migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_163809) do
+ActiveRecord::Schema.define(version: 2021_02_20_083751) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
