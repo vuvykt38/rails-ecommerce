28ad66a14b690529b7f7c1ea21127b26939896fb
Vy <vuvykt38@gmail.com>
2021-04-03 00:06:17 +0700

    Revert "add User"
    This reverts commit 62018f6c87cccba1309d09e0808ae3b59fff28f3.

--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  has_secure_password
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   resources :orders
   resources :line_items
   resources :carts
