3c123bf90afe71f469e03ba155b342a69bdec3d0
Vy <vuvykt38@gmail.com>
2021-06-09 22:28:18 +0700

    set up new host to test FB messenger

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,6 @@ Rails.application.configure do
 
   config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "localhost.ssl"
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << "vuvykt38-local.jointunnel.com"
+  config.action_mailer.default_url_options = { host: 'vuvykt38-local.jointunnel.com', port: 443 }
 end
