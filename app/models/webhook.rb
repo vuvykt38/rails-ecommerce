88f3c2f7652dff9baa29c86d5112aaed6ab67a58
Vy <vuvykt38@gmail.com>
2021-06-13 15:50:47 +0700

    refactor by using case

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -12,24 +12,37 @@ class Webhook < ApplicationRecord
   end
 
   def handle_postback(sender_id, webhook_event)
-    payload = webhook_event["postback"]["payload"]
-    if payload == "CARE_HELP"
+    user_selected_option = webhook_event["postback"]["payload"]
+    case user_selected_option
+    when "CARE_HELP"
       send_message(MessengerResponses::CareHelp.new.call(sender_id))
-    elsif payload == "get_started"
+    when "get_started"
       sender_id = webhook_event["sender"]["user_ref"]
       send_message(MessengerResponses::Greeting.new.call(sender_id))
       send_message(MessengerResponses::TextFromUser.new.call_2(sender_id))
       send_message(MessengerResponses::TextFromUser.new.call_3(sender_id))
+    when "ORDER_UPDATES"
+      send_message(MessengerResponses::OrderUpdates.new.call(sender_id))
     end
   end
 
   def handle_message(sender_id, webhook_event)
     received_message = webhook_event["message"]["text"]
-    if received_message == "Order Inquiries"
+    case received_message
+    when "Order Inquiries", "Billing Issues", "Other"
       send_message(MessengerResponses::OrderInquiries.new.call_1(sender_id, received_message))
       send_message(MessengerResponses::OrderInquiries.new.call_2(sender_id))
       send_message(MessengerResponses::OrderInquiries.new.call_3(sender_id))
-
+    when "Talk to an agent"
+      send_message(MessengerResponses::CareHelp.new.call(sender_id))
+    when "😀"
+      send_message(MessengerResponses::Feedback.new.call_1(sender_id))
+    when "🙂"
+      send_message(MessengerResponses::Feedback.new.call_2(sender_id))
+    when "🙁"
+      send_message(MessengerResponses::Feedback.new.call_3(sender_id))
+    when "#"
+      send_message(MessengerResponses::Feedback.new.call_4(sender_id))
     else
       send_message(MessengerResponses::TextFromUser.new.call_1(sender_id, received_message))
       send_message(MessengerResponses::TextFromUser.new.call_2(sender_id))
