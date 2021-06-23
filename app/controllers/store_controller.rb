3bf777031832c05bbe9e4eaf1a59d1ecb5409dab
Vy <vuvykt38@gmail.com>
2021-06-24 00:01:03 +0700

    add category to sidebar to filter product by category

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,7 +3,9 @@ class StoreController < ApplicationController
   before_action :set_cart
   def index
     @products = Product.order(:title)
-    if params[:term]
+    if params[:category]
+      @products = Product.where(category: params[:category])
+    elsif params[:term]
       @products = Product.where(["title LIKE ? OR description LIKE ?", "%#{params[:term]}%", "%#{params[:term]}%"])
     else
       @products = Product.all
