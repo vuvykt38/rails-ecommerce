791ccc4638570ea5e756d428f4676fcb5d3f260a
Vy <vuvykt38@gmail.com>
2021-07-07 00:41:19 +0700

    save last_cart_item under sessionStorage for GTM usages

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to store_index_url}
+        format.html {
+          redirect_to store_index_url}
         format.js
         format.json { render :show, status: :created, location: @line_item }
       else
